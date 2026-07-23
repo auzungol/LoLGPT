1605b94578d9b365022a0a9427cf66eb04a951de
Ahmet Uzungöl <ahmetuzungl@gmail.com>
2026-07-23 12:57:28 +0300

    İlk taahhüt

---
+++ src/main.py
@@ -0,0 +1,4 @@
+from config import CHAT_MODEL, EMBEDDING_MODEL
+
+print("Chat Model :", CHAT_MODEL)
+print("Embedding :", EMBEDDING_MODEL)
---
+++ src/rag.py
@@ -0,0 +1,7 @@
+CHAT_MODEL = "phi-3.5-mini"
+
+EMBEDDING_MODEL = "qwen3-embedding-0.6b"
+
+DATABASE_PATH = "../database/champions.db"
+
+CHAMPION_FOLDER = "../data/champions"
